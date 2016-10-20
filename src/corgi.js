0d8d9a5b4b10b5e62e7ad11de5ed09bbe8ab8205
ryands <ryan@ryands.org>
2016-10-20 08:48:48 -0700

    let us know when an error occurs

--- src/corgi.js
+++ src/corgi.js
@@ -1,7 +1,17 @@
 var corggit = require('../lib/corggit');
 
 module.exports = (robot) => {
-  robot.respond(/corgi/, (res) => {
-    return corggit().then(post => res.send(post.url));
+  robot.respond(/corgi/, res => {
+    return corggit().then(post => res.send(post.url)).catch(e => {
+      res.send({
+        text: 'Error: Couldn\'t corgi :(',
+        attachments: [
+          {
+            "text": e,
+            "color": "#FF0000"
+          }
+        ]
+      });
+    });
   });
 };
